1cc0d7e23effee610566c989952298f032f3afb0
xyzuan <xyzuannihboss@gmail.com>
2023-07-27 15:33:27 +0700

    main: Automatically print generated docs

--- mainClient.js
+++ mainClient.js
@@ -1,16 +1,12 @@
-import WhatsAppWeb from 'whatsapp-web.js';
-import clientAuth from './lib/client/clientAuth.js';
-import clientHandler from './lib/client/clientHandler.js';
-import clientDebugger from './lib/client/clientDebugger.js';
+import WhatsAppWeb from "whatsapp-web.js";
+import clientAuth from "./lib/client/clientAuth.js";
+import clientHandler from "./lib/client/clientHandler.js";
+import clientDebugger from "./lib/client/clientDebugger.js";
 
-export var {
-    Client,
-    LocalAuth,
-    MessageMedia
-} = WhatsAppWeb;
+export var { Client, LocalAuth, MessageMedia } = WhatsAppWeb;
 
 var client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
 });
 
 clientAuth(client);
