3d2a20836180d869398be10e810bcf0293d1f66a
xyzuan <xyzuannihboss@gmail.com>
2023-07-29 23:31:11 +0700

    main: Optimize Web Dashboard

--- public/index.ejs.js
+++ public/index.ejs.js
@@ -1,49 +1,71 @@
 document.addEventListener("click", async function (event) {
   if (event.target && event.target.classList.contains("btnPrint")) {
-    const skType = event.target.dataset.sktype;
-    const nama = event.target.dataset.nama;
-    const nik = event.target.dataset.nik;
-    const ttl = event.target.dataset.ttl;
-    const agama = event.target.dataset.agama;
-    const bekerja = event.target.dataset.bekerja;
-    const alamat = event.target.dataset.alamat;
-    const datetime = event.target.dataset.datetime;
+    if (confirm(`Yakin untuk acc data ini? `)) {
+      const skType = event.target.dataset.sktype;
+      const nama = event.target.dataset.nama;
+      const nik = event.target.dataset.nik;
+      const ttl = event.target.dataset.ttl;
+      const agama = event.target.dataset.agama;
+      const bekerja = event.target.dataset.bekerja;
+      const alamat = event.target.dataset.alamat;
+      const datetime = event.target.dataset.datetime;
 
-    const response = await fetch("/genDoc", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        skType,
-        nama,
-        nik,
-        ttl,
-        agama,
-        bekerja,
-        alamat,
-        datetime,
-      }),
-    });
-  } else if (event.target && event.target.classList.contains("btnReject")) {
-    const dataId = event.target.dataset.id;
-    if (!dataId || isNaN(dataId)) {
-      return;
+      await fetch("/genDoc", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          skType,
+          nama,
+          nik,
+          ttl,
+          agama,
+          bekerja,
+          alamat,
+          datetime,
+        }),
+      });
     }
+  } else if (event.target && event.target.classList.contains("btnReject")) {
+    const adminReason = window.prompt(`Alasan permintaan surat ditolak? `);
+    const dataId = event.target.dataset.id;
+    const notelp = event.target.dataset.notelp;
+    if (adminReason !== null) {
+      if (
+        confirm(`Yakin untuk menolak permintaan surat? Alasan: ${adminReason}`)
+      ) {
+        const msg = `Perminataan Surat Anda ditolak, ${adminReason}`;
+        if (!dataId || isNaN(dataId)) {
+          return;
+        }
 
-    const response = await fetch(`/deleteData/${dataId}`, {
-      method: "DELETE",
-    });
+        const response = await fetch(`/deleteData/${dataId}`, {
+          method: "DELETE",
+        });
 
-    if (response.ok) {
-      window.location.reload();
+        if (response.ok) {
+          window.location.reload();
+        }
+
+        fetch("/sendMsg", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            notelp,
+            msg,
+          }),
+        });
+      }
     }
   } else if (event.target && event.target.classList.contains("btnDone")) {
     const dataId = event.target.dataset.id;
     const notelp = event.target.dataset.notelp;
     const msg = "Surat sudah siap diambil di Balai Desa";
 
-    const response = fetch("/sendMsg", {
+    fetch("/sendMsg", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -54,7 +76,7 @@ document.addEventListener("click", async function (event) {
       }),
     });
 
-    const deleteResponse = fetch(`/deleteData/${dataId}`, {
+    fetch(`/deleteData/${dataId}`, {
       method: "DELETE",
     });
     window.location.reload();
