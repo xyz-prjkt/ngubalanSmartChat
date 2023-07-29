f0f5132b34f05620b55a0ad665426bdf4c81a1c1
xyzuan <xyzuannihboss@gmail.com>
2023-07-29 20:49:11 +0700

    main: Create Admin Page to validate userData value

--- mainClient.js
+++ mainClient.js
@@ -2,13 +2,15 @@ import WhatsAppWeb from "whatsapp-web.js";
 import clientAuth from "./lib/client/clientAuth.js";
 import clientHandler from "./lib/client/clientHandler.js";
 import clientDebugger from "./lib/client/clientDebugger.js";
+import clientAPI from "./lib/client/clientAPI.js";
 
 export var { Client, LocalAuth, MessageMedia } = WhatsAppWeb;
 
-var client = new Client({
+export var client = new Client({
   authStrategy: new LocalAuth(),
 });
 
+clientAPI();
 clientAuth(client);
 clientDebugger(client);
 clientHandler(client);
