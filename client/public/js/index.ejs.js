1e2cb22d95c3b6fb1d0a62ac5d9492d7508c6549
xyzuan <xyzuannihboss@gmail.com>
2023-07-31 17:25:12 +0700

    main: Fixup SKIK Data generate

--- client/public/js/index.ejs.js
+++ client/public/js/index.ejs.js
@@ -2,30 +2,63 @@ document.addEventListener("click", async function (event) {
   if (event.target && event.target.classList.contains("btnPrint")) {
     if (confirm(`Yakin untuk acc data ini? `)) {
       const skType = event.target.dataset.sktype;
-      const nama = event.target.dataset.nama;
-      const nik = event.target.dataset.nik;
-      const ttl = event.target.dataset.ttl;
-      const agama = event.target.dataset.agama;
-      const bekerja = event.target.dataset.bekerja;
-      const alamat = event.target.dataset.alamat;
       const datetime = event.target.dataset.datetime;
+      let response;
+      if (skType === "SKTM") {
+        const nama = event.target.dataset.nama;
+        const nik = event.target.dataset.nik;
+        const ttl = event.target.dataset.ttl;
+        const agama = event.target.dataset.agama;
+        const bekerja = event.target.dataset.bekerja;
+        const alamat = event.target.dataset.alamat;
 
-      const response = await fetch("/genDoc", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          skType,
-          nama,
-          nik,
-          ttl,
-          agama,
-          bekerja,
-          alamat,
-          datetime,
-        }),
-      });
+        response = await fetch("/genDoc", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            skType,
+            nama,
+            nik,
+            ttl,
+            agama,
+            bekerja,
+            alamat,
+            datetime,
+          }),
+        });
+      } else if (skType === "SKIK") {
+        const namaOrtu = event.target.dataset.namaortu;
+        const ttlOrtu = event.target.dataset.ttlortu;
+        const alamatOrtu = event.target.dataset.alamatortu;
+        const nikOrtu = event.target.dataset.nikortu;
+        const nama = event.target.dataset.nama;
+        const ttl = event.target.dataset.ttl;
+        const alamat = event.target.dataset.alamat;
+        const nik = event.target.dataset.nik;
+        const destination = event.target.dataset.destination;
+
+        response = await fetch("/genDoc", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            skType,
+            namaOrtu,
+            ttlOrtu,
+            alamatOrtu,
+            nikOrtu,
+            nama,
+            ttl,
+            alamat,
+            nik,
+            destination,
+            datetime,
+          }),
+        });
+      }
 
       if (response.ok) {
         const blob = await response.blob();
